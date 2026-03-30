20bd3d8eba605c85381de2f69e9bf7d8e1426748
Samuel Soumi <samuel_somy93@hotmail.com>
2026-03-30 14:36:49 +0200

    Adjsut User_Scema and create a User_model

--- src/models/User.js
+++ src/models/User.js
@@ -4,15 +4,28 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    lowercase: true
   },
   password: {
     type: String,
     required: true,
+    minlength: 8,
+    // Regex: Minst 8 tecken, en stor, en liten, en siffra, ett specialtecken
+    match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 
+    'Lösenordet är för svagt. Kräver 8 tecken, stor/liten bokstav, siffra och symbol.']
   },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timpstamps: true
 });
 
 userSchema.pre("save", async function (next) {
@@ -23,6 +36,6 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
