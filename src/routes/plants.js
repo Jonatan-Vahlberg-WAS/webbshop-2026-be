0d9ab93a00402a2ee2797de0768a5451495e972b
Ebba-Ene <eneqvist.ebba@gmail.com>
2026-03-31 13:42:22 +0200

    updated router to plantRouter

--- src/routes/plants.js
+++ src/routes/plants.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { validatePlant, validatePlantResult } from "../middleware/plantValidation.js";
 import { createPlant, getPlants } from "../db/plant.js";
-const router = Router();
+const plantRouter = Router();
 
-router.get("/", async (req, res) => {
+plantRouter.get("/", async (req, res) => {
   const plants = await getPlants();
   res.json(plants);
 });
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 
 //TODO GET /plants/:slug
 
-router.post("/", validatePlant, validatePlantResult, async (req, res) => {
+plantRouter.post("/", validatePlant, validatePlantResult, async (req, res) => {
   const plant = await createPlant(req.body);
   res.status(201).json(plant);
 });
@@ -20,4 +20,4 @@ router.post("/", validatePlant, validatePlantResult, async (req, res) => {
 //TODO PUT /plants/:slug
 
 //TODO DELETE /plants/:slug
-export default router;
+export default plantRouter;
