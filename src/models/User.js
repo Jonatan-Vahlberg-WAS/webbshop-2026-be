e2145a5dd2d25785a59740dedf42c3b6bc8e2fd0
Desirée Strand <desireestrxnd@gmail.com>
2026-03-31 11:07:41 +0200

    update user schema
    Co-authored-by: Marcus <codedcollections@users.noreply.github.com>
    Co-authored-by: EmmyW <emmywennersten@users.noreply.github.com>

--- src/models/User.js
+++ src/models/User.js
@@ -4,15 +4,51 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
+    trim: true,
+    select: false,
   },
+  resetPasswordCode: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    required: true,
+    default: "user",
+  },
+  address: {
+    type: String,
+    trim: true,
+  },
+  plants: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Plant",
+      required: false,
+    },
+  ],
+  history: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Trade",
+      required: false,
+    },
+  ],
+}, {
+  timestamps: true
 });
 
 userSchema.pre("save", async function (next) {
