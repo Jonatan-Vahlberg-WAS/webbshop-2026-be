029476f4f008c0e1b9c43356097d90a4c4ea96b9
codedcollections <ma.unander@gmail.com>
2026-03-31 14:02:45 +0200

    Update trade schema to save ownerId matching to Plants ownerId

--- src/models/Trade.js
+++ src/models/Trade.js
@@ -1,27 +1,51 @@
 import mongoose from "mongoose"
-
+import Plant from "./Plant.js"
 export const STATUS_LEVEL = {
   pending: "pending",
   approved: "approved",
   completed: "completed",
 }
 
-const tradeSchema = new mongoose.Schema({
-  ownerId: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Plants",
-    required: false, //Change to true later
+const tradeSchema = new mongoose.Schema(
+  {
+    plantId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Plants",
+      required: false, //Change to true later
+    },
+    requesterId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+      required: false, //Change to true later
+    },
+    ownerId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+      required: false, //Change to true later
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: [
+        STATUS_LEVEL.pending,
+        STATUS_LEVEL.approved,
+        STATUS_LEVEL.completed,
+      ],
+    },
   },
-  requesterId: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "User",
-    required: false, //Change to true later
+  {
+    timestamps: true,
   },
-  status: {
-    type: String,
-    required: true,
-    enum: [STATUS_LEVEL.pending, STATUS_LEVEL.approved, STATUS_LEVEL.completed],
-  },
+)
+
+tradeSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("plantId")) {
+    const plant = await Plant.findById(this.plantId).select("ownerId")
+    if (plant) {
+      this.ownerId = plant.ownerId
+    }
+  }
+  next()
 })
 
 const Trade = mongoose.model("Trade", tradeSchema)
