d1232842b48fb77b4c08c97b11aadb1b6a67b209
codedcollections <ma.unander@gmail.com>
2026-04-01 14:44:53 +0200

    Create GET POST PUT DELETE routes for Plants Co-authored-by: EbbaE <eneqvist.ebba@gmail.com> Co-authored-by: Desirée Strand <desireestrand@users.noreply.github.com>

--- src/db/plant.js
+++ src/db/plant.js
@@ -1,18 +1,73 @@
-import Plant from "../models/Plant.js";
+import Plant from "../models/Plant.js"
 
 export async function getPlants() {
-  return await Plant.find();
+  return await Plant.find()
+}
+
+export async function getPlantById(id) {
+  try {
+    return await Plant.findById(id)
+  } catch (err) {
+    console.error("Unable to read from 'Plants'", err)
+    return null
+  }
+}
+
+export async function getPlantBySlug(slug) {
+  //   console.log("Slug: ", slug)
+  try {
+    // console.log("Found plant by slug: ", Plant.findOne({ slug: slug }))
+    return await Plant.findOne({ slug: slug })
+  } catch (err) {
+    console.error("Unable to read from 'Plants'", err)
+    return null
+  }
 }
 
 export async function createPlant(plantData) {
-    try {
-        const plant = new Plant(plantData);
-        await plant.save();
-        return plant;
-    } catch (error) {
-        console.error("Error creating plant:", error);
-        throw error;
-    }
+  try {
+    const newPlant = new Plant(plantData)
+    await newPlant.save()
+    return newPlant
+  } catch (error) {
+    console.error("Error creating 'Plant':", error)
+    throw error
+  }
 }
+
+export async function updatePlantBySlug(slug, plantData) {
+  try {
+    return await Plant.findOneAndUpdate({ slug: slug }, plantData, {
+      new: true,
+    })
+  } catch (error) {
+    console.error("Error Updating 'Plant':", error)
+    throw error
+  }
+}
+
+export async function deletePlantBySlug(slug) {
+  try {
+    const plantToDelete = await Plant.findOne({ slug: slug })
+    if (!plantToDelete) return null
+    await Plant.deleteOne({ _id: plantToDelete._id })
+    return true
+  } catch (error) {
+    console.error("Unable to delete 'Plant'", error)
+    return false
+  }
+}
+
+// export async function deletePlant(id) {
+//   try {
+//     const plantToDelete = await Plant.findById(id)
+//     if (!plantToDelete) return null
+//     await Plant.deleteOne({ _id: plantToDelete._id })
+//     return true
+//   } catch (error) {
+//     console.error("Unable to delete 'Plant'", error)
+//     return false
+//   }
+// }
 
 //TODO: Add more functions as needed
--- src/routes/plants.js
+++ src/routes/plants.js
@@ -3,7 +3,15 @@ import {
   validatePlant,
   validatePlantResult,
 } from "../middleware/plantValidation.js"
-import { createPlant, getPlants } from "../db/plant.js"
+import {
+  createPlant,
+  getPlants,
+  // getPlantById,
+  // deletePlant,
+  getPlantBySlug,
+  deletePlantBySlug,
+  updatePlantBySlug,
+} from "../db/plant.js"
 const plantRouter = Router()
 
 plantRouter.get("/", async (req, res) => {
@@ -13,15 +21,85 @@ plantRouter.get("/", async (req, res) => {
 
 //TODO: Add more routes as needed
 
-//TODO GET /plants/:slug
-
 plantRouter.post("/", async (req, res) => {
+  //kommer ha validering för user och admin
   //validatePlant, validatePlantResult,
   const plant = await createPlant(req.body)
   res.status(201).json(plant)
 })
 
+//TODO GET /plants/:slug
+plantRouter.get("/:slug", async (req, res) => {
+  const slug = req.params.slug
+  const foundPlant = await getPlantBySlug(slug)
+  if (!foundPlant) {
+    return res.status(404).json({
+      message: "Plant not found",
+    })
+  }
+  res.json(foundPlant)
+})
 //TODO PUT /plants/:slug
+plantRouter.put("/:slug", async (req, res) => {
+  const slug = req.params.slug
+  const { name, image, species, lightLevels, meetingTime } = req.body
+
+  if (!name || !image || !species || !lightLevels || !meetingTime) {
+    return res.status(400).json({
+      message:
+        "All fields (name, image, species, lightLevels, and meetingTime) are required",
+    })
+  }
+  const updatedPlant = await updatePlantBySlug(slug, {
+    name,
+    image,
+    species,
+    lightLevels,
+    meetingTime,
+  })
+  if (!updatedPlant) {
+    return res.status(404).json({
+      message: "Plant does not exist",
+    })
+  }
+
+  return res.status(200).json(updatedPlant)
+})
 
 //TODO DELETE /plants/:slug
+plantRouter.delete("/:slug", async (req, res) => {
+  //kommer ha validering för user och admin
+  const slug = req.params.slug
+  const plant = await deletePlantBySlug(slug)
+  if (!plant) {
+    return res.status(400).json({
+      message: "Plant does not exist",
+    })
+  }
+  return res.status(204).json()
+})
+
+// plantRouter.get("/:id", async (req, res) => {
+//   const id = req.params.id
+//   const foundPlant = await getPlantById(id)
+//   if (!foundPlant) {
+//     return res.status(404).json({
+//       message: "Plant not found",
+//     })
+//   }
+//   res.json(foundPlant)
+// })
+
+// plantRouter.delete("/:id", async (req, res) => {
+//   //kommer ha validering för user och admin
+//   const id = req.params.id
+//   const plant = await deletePlant(id)
+//   if (!plant) {
+//     return res.status(400).json({
+//       message: "Plant does not exist",
+//     })
+//   }
+//   return res.status(204).json()
+// })
+
 export default plantRouter
