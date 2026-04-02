f3e66015c3b54ec34f8172bab53dbdc076c0bc9f
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2026-04-02 13:41:41 +0200

    Added reqrole admin types

--- src/routes/types.js
+++ src/routes/types.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import TypesController from '../controllers/Types.js';
 import { isAuth } from '../middleware/authMiddleware.js';
+import requiredRole from '../middleware/roleMiddleware.js';
 
 const router = Router();
 const typesController = new TypesController();
 
 router.get('/', typesController.typesGet);
-router.post('/', isAuth, typesController.typesPost);
-router.put('/:id', isAuth, typesController.editTypePut);
-router.delete('/:id', isAuth, typesController.typesDelete);
+router.post('/', isAuth, requiredRole('admin'), typesController.typesPost);
+router.put('/:id', isAuth, requiredRole('admin'), typesController.editTypePut);
+router.delete(
+  '/:id',
+  isAuth,
+  requiredRole('admin'),
+  typesController.typesDelete
+);
 
 export default router;
