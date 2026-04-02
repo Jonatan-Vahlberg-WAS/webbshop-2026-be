d5f1bebfa55cc94a9344e9b5822cc01aadb0424b
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2026-04-02 13:38:59 +0200

    removed isAdmin

--- src/routes/types.js
+++ src/routes/types.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import TypesController from '../controllers/Types.js';
-import { isAuth, isAdmin } from '../middleware/authMiddleware.js';
+import { isAuth } from '../middleware/authMiddleware.js';
 
 const router = Router();
 const typesController = new TypesController();
 
 router.get('/', typesController.typesGet);
-router.post('/', isAuth, isAdmin, typesController.typesPost);
-router.put('/:id', isAuth, isAdmin, typesController.editTypePut);
-router.delete('/:id', isAuth, isAdmin, typesController.typesDelete);
+router.post('/', isAuth, typesController.typesPost);
+router.put('/:id', isAuth, typesController.editTypePut);
+router.delete('/:id', isAuth, typesController.typesDelete);
 
 export default router;
