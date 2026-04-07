ad018e3106b79ca26f26e1a558f8b37f06210cfe
Ludvig <74721111+Ludvig-D@users.noreply.github.com>
2026-04-07 16:16:02 +0200

    Added more ports

--- src/app.js
+++ src/app.js
@@ -20,7 +20,12 @@ async function connectDB() {
 }
 
 // Change to frontend URL when ready
-const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
+const allowedOrigins = [
+  'http://127.0.0.1:3000',
+  'http://localhost:3000',
+  'http://127.0.0.1:5500',
+  'http://localhost:5500',
+];
 
 // Middleware
 app.use(
