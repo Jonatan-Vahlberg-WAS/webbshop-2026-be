32c709772eb699fa1e8bb58036c6bbdf11e17092
AishaH <aisha_1@live.se>
2026-04-07 09:55:10 +0200

    Update CORS middleware to specify allowed methods and enable credentials

--- src/app.js
+++ src/app.js
@@ -29,7 +29,9 @@ app.use(
   cors({
     origin: [
       "http://127.0.0.1:5500"
-    ]
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true 
   })
 );
 app.use(express.json());
