70fd7cfd5024569ff3f692b9e0ba345a592727ce
Regron95 <rronesen@outlook.com>
2026-04-10 12:37:00 +0200

    fix bugged code

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import authRouter from "./routes/auth.js";
 import userRouter from "./routes/user.js";
