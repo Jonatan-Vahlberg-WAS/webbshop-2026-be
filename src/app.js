4b16ee0e8aab87d74275c509e4fae039bc4d4b33
Regron95 <rronesen@outlook.com>
2026-04-10 12:36:12 +0200

    add back connect middleware

--- src/app.js
+++ src/app.js
@@ -11,6 +11,24 @@ import tradeRouter from "./routes/trade.js";
 
 const app = express();
 
+let isConnected = false;
+
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGODB_URI);
+  isConnected = true;
+}
+
+// Middleware
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
